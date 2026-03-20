740415b0aadc2af8b56ebda44a801cdd58df17b8
davidzakii <davidzaki77@gmail.com>
2026-03-20 20:34:56 +0200

    Fix:dynamic page and changed for it rendermode server

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'chat/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'reset-password/:token',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
