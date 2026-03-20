f3d259fa43068b94c3f93524041af257c4346e49
davidzakii <davidzaki77@gmail.com>
2026-03-20 22:04:01 +0200

    Edit:add api after production link to fit backend api

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://cuddly-silvia-webdeveloperog-1baa189a.koyeb.app',
+  apiBaseUrl: 'https://cuddly-silvia-webdeveloperog-1baa189a.koyeb.app/api',
   apiVersion: 'v1'
 };
